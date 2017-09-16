307c17d3c7005f919c62105e0f91676bceea4565
luowen <bigpao.luo@gmail.com>
2017-09-16 11:19:27 +0800

    follow biz

--- app/constants.go
+++ app/constants.go
@@ -1,14 +1,16 @@
 package app
 
 // APP_CONFIG_PATH
-const APP_CONFIG_PATH  = "../conf/app.toml"
+const APP_CONFIG_PATH  = "conf/app.toml"
 
 // DB_USERS_DATA
 const DB_USERS_DATA  = "users_data"
 
-// REDIS_SOCIAL
+// REDIS_SOCIAL social redis key name
 const REDIS_SOCIAL  =  "social"
 
+// USER_FOLLOW_TABLE_PREFIX
 const USER_FOLLOW_TABLE_PREFIX  = "user_follow_"
 
+// USER_FOLLOW_SPLIT_TABLE_NUM
 const USER_FOLLOW_SPLIT_TABLE_NUM  =  10
