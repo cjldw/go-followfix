c57faf935c86b166359accc5bc0262ee2e4a48e6
luowen <bigpao.luo@gmail.com>
2017-10-17 10:59:49 +0800

    only process anchor data

--- app/followservice.go
+++ app/followservice.go
@@ -218,7 +218,7 @@ func getUIDFansCnt(uid int) int {
 func (followService *FollowService) processSplitTable(tablename string) {
 	dbUsersData, err := GetApp().dbmgr.GetDbByName(DB_USERS_DATA)
 	CheckErr(err)
-	sql := fmt.Sprintf("select uid, anchor from %s where isFriends = 0 and status = 1", tablename)
+	sql := fmt.Sprintf("select uid, anchor from %s where isAnchor = 1 and isFriends = 0 and status = 1", tablename)
 	/*
 		if !followService.excludeUIDSet.IsEmpty() { // exclude has process uid
 			uidList1 := followService.excludeUIDSet.List()
