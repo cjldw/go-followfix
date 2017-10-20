09c91192ae28d518b7577c0abb4c51b0ac4debc2
luowen <bigpao.luo@gmail.com>
2017-10-20 18:10:54 +0800

    contents

--- app/dbmgr_test.go
+++ app/dbmgr_test.go
@@ -27,24 +27,41 @@ func TestQuery(t *testing.T)  {
 	t.Log(conf)
 	dbmgr := NewDbMgr()
 	dbmgr.InitializeDbList(conf.DbConf)
-	pool := make(chan int, 100)
-	for  {
-		pool <- 10
+	db, err := dbmgr.GetDbByName(DB_USERS_DATA)
+	CheckErr(err)
+	rows, err := db.Query("select id, uid from user_follow")
+	defer rows.Close()
 
-		db, err := dbmgr.GetDbByName(DB_USERS_DATA)
-		CheckErr(err)
-		rows, err := db.Query("select id, uid from user_follow limit 10")
-		defer rows.Close()
-		CheckErr(err)
-
+	for rows.Next() {
 		var (
 			id int
 			uid int
 		)
+		rows.Scan(&id)
+		rows.Scan(&uid)
+
+		t.Log(id, uid)
+	}
+
+	return
+
+	return;
+	pool := make(chan int, 100)
+	for  {
+		pool <- 10
+		db, err := dbmgr.GetDbByName(DB_CONTENTS)
+		CheckErr(err)
+		rows, err := db.Query("select id, name from games")
+		CheckErr(err)
+		var (
+			id int
+			name string
+		)
 		for rows.Next() {
 			rows.Scan(&id)
-			rows.Scan(&uid)
-			fmt.Println(fmt.Sprintf("UID【%d】 ID【%d】"), id, uid)
+			rows.Scan(&name)
+			fmt.Println(fmt.Sprintf("UID【%d】 ID【%s】", id, name));
 		}
+		rows.Close()
 	}
 }
